5f27b5efb572994eb4136c8988f55f6674085d5b
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-11-03 21:39:45 +0530

    Update broadcast.py

--- db/broadcast.py
+++ db/broadcast.py
@@ -14,11 +14,11 @@ from pyrogram.errors import (
     UserIsBlocked,
 )
 
-import config
+import info
 
 broadcast_ids = {}
 
-BROADCAST_AS_COPY = config.BROADCAST_AS_COPY
+BROADCAST_AS_COPY = info.BROADCAST_AS_COPY
 
 
 async def send_msg(user_id, message):
